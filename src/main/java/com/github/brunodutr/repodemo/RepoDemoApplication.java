296659c3b9dc68c298e32a8dcdbcdded74af7f52
Bruno Dutra Franco <brun_dutra@hotmail.com>
2021-09-24 15:24:12 -0300

    first commit

---
+++ src/main/java/com/github/brunodutr/repodemo/RepoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.brunodutr.repodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/brunodutr/repodemo/RepoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.brunodutr.repodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
